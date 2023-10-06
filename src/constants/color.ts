4b36f8ba15c5a62fb1d7aeae0a7d79d9321fc031
opensea712 <freedompeace9623@gmail.com>
2023-10-07 03:28:02 +0900

    Added constants for colors, fonts, sizes

---
+++ src/constants/color.ts
@@ -0,0 +1,26 @@
+export enum Color  {
+    // main colors
+    $white = 'white',
+
+    //
+    $bg_primary = '#151416',
+    $dark_primary = '#1f2128',
+    $dark_card = '#1f2128',
+    $dark_card_bright = '#313442',
+    $dark_button = '#2c2f39',
+    $pink_primary = '#7f56d9',
+    $pink_dark = '#8758ff',
+    $pink_button = '#8758ff',
+    $gray_dark = '#475467',
+    $gray_600 = '#475467',
+    $gray_primary = '#7d7d7d',
+    $gray_light = '969696',
+    $yello_primary = '#FEEC4B',
+    $brown_primary = '#FF8845',
+    $brown_light = '#EC8C56',
+    $blue_light = '#72C2FC',
+    $red_light = '#F17F7F',
+    $red_primary = '#FF8845',
+    $red_dark = '#A81E1E',
+    $green_dark = '#027A48',
+}
---
+++ src/constants/font.ts
@@ -0,0 +1,23 @@
+export enum FontSize {
+    xxs = '10px',
+    xs = '11px',
+    s = '12px',
+    base = '14px',
+    l = '16px',
+    xl = '18px',
+    xxl = '20px',
+    xxxl = '24px',
+    lxl = '32px',
+}
+
+export enum FontStyle {
+    xxs = '2px',
+    xs = '4px',
+    s = '8px',
+    base = '16px',
+    l = '24px',
+    xl = '32px',
+    xxl = '48px',
+    xxxl = '64px',
+    lxl = '80px',
+}
