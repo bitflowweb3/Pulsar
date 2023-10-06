a00d7927b89abc5fd8d1aac63fc5db426333c515
opensea712 <freedompeace9623@gmail.com>
2023-10-07 02:17:02 +0900

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
-  }
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
 }
 module.exports = nextConfig
