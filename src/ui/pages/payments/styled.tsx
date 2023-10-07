827721ebb6332beb5686b1fec723f68ca71dcede
opensea712 <freedompeace9623@gmail.com>
2023-10-07 22:03:47 +0900

    Added ipaddress & payments & settings page

---
+++ src/ui/pages/payments/styled.tsx
@@ -0,0 +1,60 @@
+import { Space } from '../../../constants/size';
+import styled from 'styled-components';
+
+const PageHeader = styled.div`
+  padding: ${Space.none} ${Space.sb};
+  padding-bottom: ${Space.s};
+  h6 {
+    color: var(--color-gray-100);
+  }
+`;
+
+const DashboardMainContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: var(--space-xl);
+  overflow-y: auto;
+  height: calc(100vh - 154px);
+  padding-left: ${Space.sb};
+
+  &::-webkit-scrollbar {
+    width: 5px;
+  }
+
+  /* Track */
+  &::-webkit-scrollbar-track {
+    background: #f1f1f1;
+  }
+
+  /* Handle */
+  &::-webkit-scrollbar-thumb {
+    background: var(--color-gray-400);
+  }
+
+  /* Handle on hover */
+  &::-webkit-scrollbar-thumb:hover {
+    background: var(--color-neutral-400);
+  }
+
+  .sub-content {
+    display: flex;
+    flex-direction: column;
+    gap: var(--space-l);
+
+    &:first-child {
+      flex-grow: 1;
+    }
+    &:last-child {
+      flex-basis: 240px;
+      flex-grow: none;
+    }
+  }
+  .dashboard-right-sidebar {
+    gap: var(--space-xxl);
+    position: sticky;
+    top: var(--space-s);
+    right: var(--space-base);
+  }
+`;
+
+export { PageHeader, DashboardMainContent };
---
+++ src/ui/pages/settings/index.tsx
@@ -0,0 +1,22 @@
+import TrafficPerServer from '../../components/cards/traffic-per-server';
+import UpcomingPayments from '../../components/cards/upcoming-payments';
+import TrafficChart from '../../components/charts/traffic-chart';
+import TrafficTable from '../../components/lists/traffic-table';
+import { MainPageContainer } from '../../../styles/global';
+import { PageHeader, DashboardMainContent } from './styled';
+
+export default function SettingsPage() {
+  return (
+    <MainPageContainer>
+      <PageHeader>
+        <h3>
+          <strong>Settings</strong>
+        </h3>
+        <h6>January 1-30, 2023</h6>
+      </PageHeader>
+      <DashboardMainContent className='dashboard-content flex justify-center pt-9'>
+       <h1>Coming Soon</h1>
+      </DashboardMainContent>
+    </MainPageContainer>
+  );
+}
