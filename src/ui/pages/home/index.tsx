e69da9dd7fad43a4cc12bd54fa0680195da0ea6b
opensea712 <freedompeace9623@gmail.com>
2023-10-10 00:27:29 +0900

    Fix some endpoint error

--- src/ui/pages/home/index.tsx
+++ src/ui/pages/home/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import apiService from '@/services/apiservice';
+import apiService from '../../../services/apiservice';
 
 export default function About() {
     const [hello, setHello] = useState<string>('');
