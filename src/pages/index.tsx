cc7dea767bd1f9d2dce341c5bcaa213bf484ab99
opensea712 <freedompeace9623@gmail.com>
2023-10-17 02:49:46 +0900

    Made some change on the page/index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import React, {useEffect} from 'react';
 import withAuth from '../auth/withAuth';
 import Home from '../ui/pages/dashboard';
+import {useRouter} from 'next/router';
 
- function HomePage() {
+function HomePage() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/dashboard')
+  }, []);
   return <Home />;
 }
 
