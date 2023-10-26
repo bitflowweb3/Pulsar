2cef129e6fb03875f03303c8392bc347e1e1d20b
opensea712 <freedompeace9623@gmail.com>
2023-10-27 02:03:37 +0900

    Added some icons

--- src/ui/components/common/SvgIcon.tsx
+++ src/ui/components/common/SvgIcon.tsx
@@ -17,5 +17,9 @@ const SvgContainer = styled.div<SvgIconProps>`
         stroke: ${props => props.stroke ? props.stroke : Color.$netural_700};
         stroke-width: ${props => props.stroke ? props.strokeWidth : '1'};
     }   
+    svg > rect {
+        fill: ${props => props.stroke ? props.stroke : Color.$netural_700};
+        stroke: ${props => props.stroke ? props.stroke : Color.$netural_700};
+    }
 `
 export { SvgContainer };
