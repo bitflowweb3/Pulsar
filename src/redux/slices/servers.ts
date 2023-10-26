bddc06fe8668bd8886c7410d482bfe0f950a143e
opensea712 <freedompeace9623@gmail.com>
2023-10-26 16:55:09 +0900

    Added redux-toolkit

--- src/redux/slices/servers.ts
+++ src/redux/slices/servers.ts
@@ -1,14 +1,24 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import apiService from '../../services/apiservice';
+import { getAccessToken } from '../../services/auth';
+
+export const fetchServerList = createAsyncThunk('server/all', async () => {
+    const accessToken = (await getAccessToken()) ?? '';
+  const res = await apiService.secureApi(accessToken).getServerList();
+  return res.data;
+});
 
 const serverSlice = createSlice({
-    name: 'server',
-    initialState: {
-        serverList: [],
-    },
-    reducers: {
-        
-    }
-})
+  name: 'server',
+  initialState: {
+    serverList: [],
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchServerList.fulfilled, (state, action) => {
+      state.serverList = action.payload.serverList;
+    });
+  },
+});
 
 export default serverSlice.reducer;
