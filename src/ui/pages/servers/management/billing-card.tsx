c0921cb4ab547173fbb8c13517c47ec7f8d90738
opensea712 <freedompeace9623@gmail.com>
2023-10-31 00:11:41 +0900

    Updated billing card

--- src/ui/pages/servers/management/billing-card.tsx
+++ src/ui/pages/servers/management/billing-card.tsx
@@ -8,67 +8,72 @@ import { IconCardButton } from './server-details';
 import PaymentsIcon from '../../../components/common/svgicons/PaymentsIcon';
 
 const BillingCard = () => {
-    return (
-        <StyledCard>
-            <Header>
-                <ChartBarIcon stroke={Color.$white} />
-                <label>Billing</label>
-            </Header>
-            <hr />
-            <Body>
-                <Info>
-                    <label>Billing Model</label>
-                    <label>Monthly</label>
-                </Info>
-                <Info>
-                    <label>Start Date</label>
-                    <label>Sep 15, 2023</label>
-                </Info>
-                <Info>
-                    <label>Unit Price</label>
-                    <label>$386/Month</label>
-                </Info>
-            </Body>
-            <hr />
-            <ChangeBillingModelButton>
-                <PaymentsIcon />
-                Change Billing Model
-            </ChangeBillingModelButton>
-        </StyledCard>
-    )
-}
+  return (
+    <StyledCard>
+      <Header>
+        <LabelDiv>
+          <ChartBarIcon stroke={Color.$white} />
+          <label>Billing</label>
+        </LabelDiv>
+        <ChangeBillingModelButton>
+          Change Billing Model
+        </ChangeBillingModelButton>
+      </Header>
+      <hr />
+      <Body>
+        <Info>
+          <label>Billing Model</label>
+          <label>Monthly</label>
+        </Info>
+        <Info>
+          <label>Start Date</label>
+          <label>Sep 15, 2023</label>
+        </Info>
+        <Info>
+          <label>Unit Price</label>
+          <label>$386/Month</label>
+        </Info>
+      </Body>
+    </StyledCard>
+  );
+};
 export default BillingCard;
 
 const Header = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: ${Space.s};
-    label {
-        font-size: 18px;
-        color: ${Color.$white};
-    }
-`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+const LabelDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: ${Space.s};
+  label {
+    font-size: 18px;
+    color: ${Color.$white};
+  }
+`;
 const Body = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: ${Space.s};
-    min-height: 120px;
-`
+  display: flex;
+  flex-direction: column;
+  gap: ${Space.s};
+  min-height: 70px;
+`;
 const Info = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    label {
-        font-size: 16px;
-        &:first-child {
-            color: ${Color.$netural_700};
-        }
-        &:last-child {
-            color: ${Color.$white};
-        }
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  label {
+    font-size: 16px;
+    &:first-child {
+      color: ${Color.$netural_700};
     }
-`
+    &:last-child {
+      color: ${Color.$white};
+    }
+  }
+`;
 const ChangeBillingModelButton = styled(IconCardButton)`
-    justify-content: center;
-`
+  justify-content: center;
+`;
