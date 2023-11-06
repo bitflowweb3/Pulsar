f47264a825bbbb300a9e8e2f1b0bbbf4195076d9
opensea712 <freedompeace9623@gmail.com>
2023-11-06 22:45:36 +0900

    Added Phonenumber input component

---
+++ src/ui/components/common/phone-input/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
+
+const PhoneInputComponent = () => {
+  return (
+    <PhoneInput
+          country={'us'}
+          value='12345678'
+          onChange={() => '123123123'}
+    />
+  );
+};
+export default PhoneInputComponent;
